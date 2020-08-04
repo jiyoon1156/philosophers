23ced970e7fda0f2bcf71e8dc466f3e917b9731d
Jiyoon Hur <jhur@c2r5s5.42seoul.kr>
2020-08-04 12:39:06 +0900

    ADD:comments

--- phil_practice.c
+++ phil_practice.c
@@ -1,15 +1,72 @@
+#include <sys/time.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <stdio.h>
 
-ft_putstr(char *s)
+int ft_strlen(char *s)
 {
-	while (*s)
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		++i;
+	return (i);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int ft_error(char *str)
+{
+	write(2, str, ft_strlen(str));
+	return (0);
+}
+
+void	ft_putnbr(int n)
+{
+	long	nb;
+
+	nb = n;
+	if (nb < 0)
 	{
-		write(1, s, 1);
-		*s++;
+		ft_putchar('-');
+		nb *= -1;
 	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10); 
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	int		length;
+	char	*result;
+
+	length = 0;
+	while (s1[length])
+		length++;
+	if (!(result = (char *)malloc(sizeof(char) * (length + 1))))
+		return (0);
+	i = 0;
+	while (i < length)
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+void ft_putstr(char *str)
+{
+	write(1, str, ft_strlen(str));
 }
 
 int	ft_atoi(const char *str)
@@ -82,7 +139,8 @@ unsigned long get_time(void)
 
 	if (gettimeofday(&tv, 0))
 		return (0);
-	//miliseconds(1000ms == 1초 == 1sec)
+	//miliseconds(1000ms == 1sec)로 표현
+	//sec-km / ms-m / microsec(usec)-mm
 	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
 
@@ -92,12 +150,6 @@ t_vars	*get_vars(void)
 	return(&vars);
 }
 
-int	init(int argc, char **argv)
-{
-	t_vars *vars;
-
-	vars = get_vars();
-}
 
 void	print_status_body(t_vars *vars, t_philo *philo, t_status status, char *phrase)
 {
@@ -106,7 +158,22 @@ void	print_status_body(t_vars *vars, t_philo *philo, t_status status, char *phra
 
 	//인덱스는 0부터 시작하니까
 	philo_no = philo->p_idx + 1;
-	
+	pthread_mutex_lock((&vars->print));//print 스레드 lock
+	time = get_time() - philo->start_time;//현재 시간 - 최초 시작 시간, 즉 흐른시간
+	pthread_mutex_lock(&vars->someone_died);
+	if (vars->flag_died)
+	{
+		pthread_mutex_unlock(&vars->someone_died);
+		return ;
+	}
+	pthread_mutex_unlock(&vars->someone_died);
+	ft_putnbr(time);
+	ft_putstr(" ");
+	ft_putnbr(philo_no);//철학자 번호
+	ft_putstr(phrase);//뭐하고 있는지
+	free(phrase);
+	if (!(status == DIED))
+		pthread_mutex_unlock(&vars->print);
 }
 
 int	print_status(t_vars *vars, t_philo *philo, t_status status)
@@ -122,12 +189,27 @@ int	print_status(t_vars *vars, t_philo *philo, t_status status)
 		phrase = ft_strdup(" is sleeping\n");
 	else if (status == DIED)
 		phrase = ft_strdup(" died\n");
+	else if (status == FORK_TAKEN)
+		phrase = ft_strdup(" has taken a fork\n");
 	if (phrase == 0)
 		return (0);
 	print_status_body(vars, philo, status, phrase);
 	return (1);
 }
+void    ft_usleep(unsigned long time)
+{
+	unsigned long start;
+	unsigned long time_elapsed;
 
+	start = get_time();
+	while (1)
+	{
+		time_elapsed = get_time() - start;
+		if (time_elapsed >= time)
+			break ;
+		usleep(1);
+	}
+}
 int	taken_fork_and_eat(t_vars *vars, t_philo *philo)
 {
 	int idx;
@@ -231,17 +313,45 @@ int free_all(int ret)
 	return (ret);
 }
 
-int	main(int argc, char **argv)
+int	init_mutexes(t_vars *vars)
+{
+	int i;
+
+	i = 0;
+	vars->n_alive = vars->n_philo; //생존자 수 == 철학자 수
+	if(!(vars->forks = malloc(sizeof(pthread_mutex_t) * vars->n_philo)))
+		return (0); //포크 수에 구조체크기 * 철학자 수만큼 malloc
+	if(!(vars->eats = malloc(sizeof(pthread_mutex_t) * vars->n_philo)))
+		return (0); //먹는 행위에 구조체크기 * 철학자 수만큼 malloc
+	//mutex 객체 초기화
+	//성공시 0 return , 아래 if 조건 default가 != 0, 따라서 실패시 return(0)이라는 뜻
+	i = 0;
+	while (i < vars->n_philo)
+		if (pthread_mutex_init(&vars->forks[i++], 0))
+			return (0);
+	i = 0;
+	while (i < vars->n_philo)
+		if (pthread_mutex_init(&vars->eats[i++], 0))
+			return (0);
+	if (pthread_mutex_init(&vars->pickup, 0))
+		return (0);
+	if (pthread_mutex_init(&vars->putdown, 0))
+		return (0);
+	if (pthread_mutex_init(&vars->alive, 0))
+		return (0);
+	if (pthread_mutex_init(&vars->print, 0))
+		return (0);
+	if (pthread_mutex_init(&vars->someone_died, 0))
+		return (0);
+	return (1);
+}
+
+int	init(int argc, char **argv)
 {
 	t_vars *vars;
 
-	if (argc != 5 || argc != 6)
-		return (-1);
 	vars = get_vars();
-	// if (!init(argc, argv) || !create_philo(vars))
-	// 	return (-1)
-
-	//init
+	//arg 예외처리, 진짜 초기화는 init mutexes에서
 	vars->n_philo = ft_atoi(argv[1]);
 	vars->t_die = ft_atoi(argv[2]);
 	vars->t_eat = ft_atoi(argv[3]);
@@ -257,72 +367,100 @@ int	main(int argc, char **argv)
 		return (0);
 	if (vars->t_die < 0 || vars->t_eat < 0 || vars->t_sleep < 0)
 		return (0);
-	
-	//init mutexes(vars)
+	if (!(init_mutexes(vars)))
+		return (0);
+	return (1);
+}
+
+int	create_philo_even(t_vars *vars, unsigned long start_time)
+{
 	int i;
-	vars->n_alive = vars->n_philo;
-	vars->forks = malloc(sizeof(pthread_mutex_t) * vars->n_philo);
-	vars->eats = malloc(sizeof(pthread_mutex_t) * vars->n_philo);
-	//mutex 객체 초기화
-	i = 0;
-	while (i < vars->n_philo)
-		pthread_mutex_init(&vars->forks[i++], 0);
-	i = 0;
-	while (i < vars->n_philo)
-		pthread_mutex_init(&vars->eats[i++], 0);
-	pthread_mutex_init(&vars->pickup, 0);
-	pthread_mutex_init(&vars->putdown, 0);
-	pthread_mutex_init(&vars->alive, 0);
-	pthread_mutex_init(&vars->print, 0);
-	pthread_mutex_init(&vars->someone_died, 0);
-
-	//create_philo(vars)
-	unsigned long start_time;
-
-	vars->philo = malloc(sizeof(t_philo) * vars->n_philo);
-	start_time = get_time();
-	//create_philo_even(vars, start_time)
-	i = 0;
 	int j;
+
+	i = 0;
 	while ((j = 2 * i) < vars-> n_philo)
 	{
 		vars->philo[j].p_idx = j;
 		vars->philo[j].n_eat = 0;
 		vars->philo[j].start_time = start_time;
 		vars->philo[j].last_eat_time = vars->philo[j].start_time;
-		pthread_create(&vars->philo[j].thread, 0, &philosophing, &vars->philo[j]);
-		pthread_detach((vars->philo[j].thread));
-		pthread_create(&vars->philo[j].m_thread, 0, &monitoring, &vars->philo[j]);
-		pthread_detach((vars->philo[j].m_thread));
+		//philosophing 하는 스레드 따로
+		if (pthread_create(&vars->philo[j].thread, 0, &philosophing, &vars->philo[j]))
+			return (ft_error("Error: cannot create pthread"));
+		if (pthread_detach((vars->philo[j].thread)))
+			return (ft_error("Error: cannot create pthread"));
+		//monitoring 하는 스레드 따로
+		if (pthread_create(&vars->philo[j].m_thread, 0, &monitoring, &vars->philo[j]))
+			return (ft_error("Error: cannot create pthread"));
+		if (pthread_detach((vars->philo[j].m_thread)))
+			return (ft_error("Error: cannot create pthread"));
 		i++;
 	}
-	//create_philo_odd(vars, start_time)
+	return (1);
+}
+
+int	create_philo_odd(t_vars *vars, unsigned long start_time)
+{
+	int i;
+	int j;
+
 	i = 0;
-	while ((j = 2 * i + 1) < vars->n_philo)
+	while ((j = 2 * i + 1) < vars-> n_philo)
 	{
 		vars->philo[j].p_idx = j;
 		vars->philo[j].n_eat = 0;
 		vars->philo[j].start_time = start_time;
 		vars->philo[j].last_eat_time = vars->philo[j].start_time;
-		pthread_create(&vars->philo[j].thread, NULL, &philosophing, &vars->philo[j]);
-		pthread_detach((vars->philo[j].thread));
-		pthread_create(&vars->philo[j].m_thread, 0, &monitoring, &vars->philo[j]);
-		pthread_detach((vars->philo[j].m_thread));
-		++i;
+		if (pthread_create(&vars->philo[j].thread, 0, &philosophing, &vars->philo[j]))
+			return (ft_error("Error: cannot create pthread"));
+		//스레드가 독립적으로 동작, 나는 기다려주지 않으니 끝나면 알아서 끝내도록 해라
+		if (pthread_detach((vars->philo[j].thread)))
+			return (ft_error("Error: cannot create pthread"));
+		if (pthread_create(&vars->philo[j].m_thread, 0, &monitoring, &vars->philo[j]))
+			return (ft_error("Error: cannot create pthread"));
+		if (pthread_detach((vars->philo[j].m_thread)))
+			return (ft_error("Error: cannot create pthread"));
+		i++;
 	}
+	return (1);
+}
+
+int	create_philo(t_vars *vars)
+{
+	unsigned long start_time;
+
+	if (!(vars->philo = malloc(sizeof(t_philo) * vars->n_philo)))
+		return (0);
+	start_time = get_time();
+	if (!(create_philo_even(vars, start_time)))
+		return (0);
+	if (!(create_philo_odd(vars, start_time)))
+		return (0);
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	t_vars *vars;
+
+	if (argc != 5 && argc != 6)
+		return (-1);
+	vars = get_vars();
+	if (!init(argc, argv) || !create_philo(vars))
+		return (-1);
 	while (1)
 	{
 		pthread_mutex_lock(&vars->alive);
-		if (vars->n_alive == 0)
+		if (vars->n_alive == 0)//먹어야 하는 최소 횟수를 다 채워서 시뮬레이션 종료되는 경우
 			break ;
 		pthread_mutex_unlock(&vars->alive);
 		pthread_mutex_lock(&vars->someone_died);
-		if (vars->flag_died == 1)
+		if (vars->flag_died == 1)//한 명이라도 죽으면 시뮬레이션 종료 (못 먹어서 죽는 경우)
 			return (free_all(0));
 		pthread_mutex_unlock(&vars->someone_died);
 		ft_usleep(5);
 	}
-	if (vars->flag_died == 0)
+	if (vars->flag_died == 0)//먹어야하는 최소횟수 다 채우면 종료
 		ft_putstr("Every philosopher ate enough!\n");
 	return(free_all(0));
 }
