a49c797b1a74ad212e70bfc375f8fd10e040d65b
jiyoonhur <jiyoon1156@gmail.com>
2020-08-06 00:17:03 +0900

    FIX:delete create philo even and odd

--- phil_two/ft_create_phils.c
+++ phil_two/ft_create_phils.c
@@ -50,66 +50,84 @@ void	*monitoring(void *v_philo)
 	return (philo);
 }
 
-int		create_philo_even(t_vars * vars, unsigned long start_time)
-{
-	int i;
-	int j;
+// int		create_philo_even(t_vars * vars, unsigned long start_time)
+// {
+// 	int i;
+// 	int j;
 
-	i = 0;
-	while ((j = 2 * i) < vars->n_philo)
-	{
-		vars->philo[j].p_idx = j;
-		vars->philo[j].n_eat = 0;
-		vars->philo[j].start_time = start_time;
-		vars->philo[j].last_eat_time = vars->philo[j].start_time;
-		if (pthread_create(&vars->philo[j].thread, NULL, &philosophing, &vars->philo[j]) != 0)
-			return (ft_error("Error: can not create pthread", 0));
-		if (pthread_detach((vars->philo[j].thread)) != 0)
-			return (ft_error("Error: can not deatch pthread", 0));
-		if (pthread_create(&vars->philo[j].m_thread, NULL, &monitoring, &vars->philo[j]) != 0)
-			return (ft_error("Error: can not create pthread", 0));
-		if (pthread_detach((vars->philo[j].m_thread)) != 0)
-			return (ft_error("Error: can not deatch pthread", 0));
-		++i;
-	}
-	return (1);
-}
+// 	i = 0;
+// 	while ((j = 2 * i) < vars->n_philo)
+// 	{
+// 		vars->philo[j].p_idx = j;
+// 		vars->philo[j].n_eat = 0;
+// 		vars->philo[j].start_time = start_time;
+// 		vars->philo[j].last_eat_time = vars->philo[j].start_time;
+// 		if (pthread_create(&vars->philo[j].thread, NULL, &philosophing, &vars->philo[j]) != 0)
+// 			return (ft_error("Error: can not create pthread", 0));
+// 		if (pthread_detach((vars->philo[j].thread)) != 0)
+// 			return (ft_error("Error: can not deatch pthread", 0));
+// 		if (pthread_create(&vars->philo[j].m_thread, NULL, &monitoring, &vars->philo[j]) != 0)
+// 			return (ft_error("Error: can not create pthread", 0));
+// 		if (pthread_detach((vars->philo[j].m_thread)) != 0)
+// 			return (ft_error("Error: can not deatch pthread", 0));
+// 		++i;
+// 	}
+// 	return (1);
+// }
 
-int		create_philo_odd(t_vars * vars, unsigned long start_time)
-{
-	int i;
-	int j;
+// int		create_philo_odd(t_vars * vars, unsigned long start_time)
+// {
+// 	int i;
+// 	int j;
 
-	i = 0;
-	while ((j = 2 * i + 1) < vars->n_philo)
-	{
-		vars->philo[j].p_idx = j;
-		vars->philo[j].n_eat = 0;
-		vars->philo[j].start_time = start_time;
-		vars->philo[j].last_eat_time = vars->philo[j].start_time;
-		if (pthread_create(&vars->philo[j].thread, NULL, &philosophing, &vars->philo[j]) != 0)
-			return (ft_error("Error: can not create pthread", 0));
-		if (pthread_detach((vars->philo[j].thread)) != 0)
-			return (ft_error("Error: can not deatch pthread", 0));
-		if (pthread_create(&vars->philo[j].m_thread, NULL, &monitoring, &vars->philo[j]) != 0)
-			return (ft_error("Error: can not create pthread", 0));
-		if (pthread_detach((vars->philo[j].m_thread)) != 0)
-			return (ft_error("Error: can not deatch pthread", 0));
-		++i;
-	}
-	return (1);
-}
+// 	i = 0;
+// 	while ((j = 2 * i + 1) < vars->n_philo)
+// 	{
+// 		vars->philo[j].p_idx = j;
+// 		vars->philo[j].n_eat = 0;
+// 		vars->philo[j].start_time = start_time;
+// 		vars->philo[j].last_eat_time = vars->philo[j].start_time;
+// 		if (pthread_create(&vars->philo[j].thread, NULL, &philosophing, &vars->philo[j]) != 0)
+// 			return (ft_error("Error: can not create pthread", 0));
+// 		if (pthread_detach((vars->philo[j].thread)) != 0)
+// 			return (ft_error("Error: can not deatch pthread", 0));
+// 		if (pthread_create(&vars->philo[j].m_thread, NULL, &monitoring, &vars->philo[j]) != 0)
+// 			return (ft_error("Error: can not create pthread", 0));
+// 		if (pthread_detach((vars->philo[j].m_thread)) != 0)
+// 			return (ft_error("Error: can not deatch pthread", 0));
+// 		++i;
+// 	}
+// 	return (1);
+// }
 
-int		create_philo(t_vars *vars)
+int     create_philo(t_vars *vars)
 {
 	unsigned long    start_time;
+	int i;
 
 	if (!(vars->philo = (t_philo *)malloc(sizeof(t_philo) * vars->n_philo)))
 		return (0);
 	start_time = get_time();
-	if (!create_philo_even(vars, start_time))
-		return (0);
-	if (!create_philo_odd(vars, start_time))
-		return (0);
+	i = 0;
+	while (i < vars->n_philo)
+	{
+		vars->philo[i].p_idx = i;
+		vars->philo[i].n_eat = 0;
+		vars->philo[i].start_time = start_time;
+		vars->philo[i].last_eat_time = vars->philo[i].start_time;
+		if (pthread_create(&vars->philo[i].thread, NULL, &philosophing, &vars->philo[i]) != 0)
+			return (ft_error("Error: can not create pthread", 0));
+		if (pthread_detach((vars->philo[i].thread)) != 0)
+			return (ft_error("Error: can not deatch pthread", 0));
+		if (pthread_create(&vars->philo[i].m_thread, NULL, &monitoring, &vars->philo[i]) != 0)
+			return (ft_error("Error: can not create pthread", 0));
+		if (pthread_detach((vars->philo[i].m_thread)) != 0)
+			return (ft_error("Error: can not deatch pthread", 0));
+		++i;
+	}
+	// if (!create_philo_even(vars, start_time))
+	// 	return (0);
+	// if (!create_philo_odd(vars, start_time))
+	// 	return (0);
 	return (1);
 }
--- study/phil_practice_2.c
+++ study/phil_practice_2.c
@@ -168,7 +168,7 @@ unsigned long    get_time(void)
 int ft_error(char *str, int ret)
 {
 	t_vars *vars;
-	
+
 	vars = get_vars();
 	sem_wait(vars->print_error);
 	write(2, str, ft_strlen(str));
@@ -283,7 +283,7 @@ void    print_status_body(t_vars *vars, t_philo *philo, t_status status, char *p
 	if ((sem_wait(vars->print) == -1))
 		ft_error("error: sem_wait\n", 1);
 	// time = get_time() - philo->start_time; // 여기가 문제였네; 시간 계산하는 타이밍 문제라고? 아님 상관없음
-	
+
 	// if ((sem_wait(vars->someone_died) == -1))
 	// 	ft_error("error: sem_wait\n", 1);
 	// if (vars->flag_died) //죽었다면
@@ -294,7 +294,7 @@ void    print_status_body(t_vars *vars, t_philo *philo, t_status status, char *p
 	// }
 	// if ((sem_post(vars->someone_died) == -1))
 	// 	ft_error("error: sem_post\n", 1);
-	
+
 	ft_putnbr(time);
 	ft_putstr_fd(" ", 1);
 	ft_putnbr(philo_no);
@@ -332,7 +332,7 @@ int     taken_fork_and_eat(t_vars *vars, t_philo *philo)
 {
 	if ((sem_wait(vars->pickup) == -1))//포크를 드는거 lock
 		ft_error("error: sem_wait\n", 1);
-	
+
 	if ((sem_wait(vars->forks) == -1))//포크 1번째꺼 lock
 		ft_error("error: sem_wait\n", 1);
 	print_status(vars, philo, FORK_TAKEN);//포크 1개 들었음!!!!!
@@ -342,7 +342,7 @@ int     taken_fork_and_eat(t_vars *vars, t_philo *philo)
 
 	if ((sem_post(vars->pickup) == -1))//포크 다들었으니까 드는거 unlock
 		ft_error("error: sem_post\n", 1);
-	
+
 	if ((sem_wait(vars->eats) == -1)) //이제 먹어야되니까 공유자원인 시간(last_eat_time) 계산해야돼서 먹는 행위 lock
 		ft_error("error: sem_wait\n", 1);
 	philo->last_eat_time = get_time();//마지막으로 먹는 시간 저장해두기
@@ -467,14 +467,32 @@ int create_philo_odd(t_vars * vars, unsigned long start_time)
 int     create_philo(t_vars *vars)
 {
 	unsigned long    start_time;
+	int i;
 
 	if (!(vars->philo = (t_philo *)malloc(sizeof(t_philo) * vars->n_philo)))
 		return (0);
 	start_time = get_time();
-	if (!create_philo_even(vars, start_time))
-		return (0);
-	if (!create_philo_odd(vars, start_time))
-		return (0);
+	i = 0;
+	while (i < vars->n_philo)
+	{
+		vars->philo[i].p_idx = i;
+		vars->philo[i].n_eat = 0;
+		vars->philo[i].start_time = start_time;
+		vars->philo[i].last_eat_time = vars->philo[i].start_time;
+		if (pthread_create(&vars->philo[i].thread, NULL, &philosophing, &vars->philo[i]) != 0)
+			return (ft_error("Error: can not create pthread", 0));
+		if (pthread_detach((vars->philo[i].thread)) != 0)
+			return (ft_error("Error: can not deatch pthread", 0));
+		if (pthread_create(&vars->philo[i].m_thread, NULL, &monitoring, &vars->philo[i]) != 0)
+			return (ft_error("Error: can not create pthread", 0));
+		if (pthread_detach((vars->philo[i].m_thread)) != 0)
+			return (ft_error("Error: can not deatch pthread", 0));
+		++i;
+	}
+	// if (!create_philo_even(vars, start_time))
+	// 	return (0);
+	// if (!create_philo_odd(vars, start_time))
+	// 	return (0);
 	return (1);
 }
 
